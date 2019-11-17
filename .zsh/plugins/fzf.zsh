03f305a04bc5349e794b5fe02c4a34695e6f29e7
Andreas Rydbrink <andreas.rydbrink@gmail.com>
2019-11-17 16:13:23 +0100

    [fzf] use os package

--- .zsh/plugins/fzf.zsh
+++ .zsh/plugins/fzf.zsh
@@ -1,14 +1,16 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == *${HOME}/.fzf/bin* ]]; then
-  export PATH="$PATH:${HOME}/.fzf/bin"
-fi
+# if [[ ! "$PATH" == *${HOME}/.fzf/bin* ]]; then
+#   export PATH="$PATH:${HOME}/.fzf/bin"
+# fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "${HOME}/.fzf/shell/completion.zsh" 2> /dev/null
+# [[ $- == *i* ]] && source "${HOME}/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/usr/share/doc/fzf/examples/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "${HOME}/.fzf/shell/key-bindings.zsh"
+# source "${HOME}/.fzf/shell/key-bindings.zsh"
+source "/usr/share/doc/fzf/examples/key-bindings.zsh"
 
